37842e37fab747fd2e0dd8322d6c431c6987d2bf
Olsen <chrisolsen1993@gmail.com>
2022-05-22 20:50:07 -0400

    pst working

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -1,13 +1,14 @@
 import {React, useState} from 'react'
 
 export default function FormComponent() {
-    const [prompt, setData] = useState([]);
+    const [data, setData] = useState([]);
+    const [response, setResponse] = useState([])
     const [loading, setLoading] = useState('false')
     console.log(process.env)
 const handleSubmit=(e) =>{
     e.preventDefault()
-    const data = {
-        prompt: "",
+    const data1 = {
+        prompt: `${data}`,
         temperature: 0.5,
         max_tokens: 64,
         top_p: 1.0,
@@ -15,6 +16,8 @@ const handleSubmit=(e) =>{
         presence_penalty: 0.0,
        };
        setLoading(true)
+      
+       
         
        fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
         method: "POST",
@@ -22,24 +25,44 @@ const handleSubmit=(e) =>{
           "Content-Type": "application/json",
           Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
         },
-        body: JSON.stringify(data),
-       }).then(()=>{
+        body: JSON.stringify(data1),
+       }).then((response)=>{
+           console.log (response)
            console.log("Succes")
+           setData(data)
+        setResponse(response.Response.choices[0].text)
+       
            setLoading(false)
        });
 }   
 
     return (
-        <div>
+        <section>
               <h2> Let's have fun with AI</h2> <br/>
        <h5> Enter prompt</h5>
        <form onSubmit={handleSubmit}>
+           
          <textarea
-         value={prompt}
-         onChange={(e)=>setData(e.target.value)}>   
+         type="text"
+        
+         placeholder="Enter Data"
+       
+         onChange={(e)=>setData(e.target.value)}
+         value={data}
+        >   
          </textarea><br/>
-         <button>Submit</button>
+         <button type="submit">Submit</button>
        </form>
-        </div>
+       <br/>
+       <br/>
+       <article>
+           <h2>Responses</h2>
+           <ul>
+               <li>Prompt: {data}</li>
+               <li>Response:{response}</li>
+           </ul>
+
+       </article>
+        </section>
     )
 }
